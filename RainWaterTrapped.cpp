2aee09b7befb186415aee51bd4c42a4e0a16ae07
Arshit Goel <arshit.goel197@gmail.com>
2019-06-13 10:33:55 +0530

    Update RainWaterTrapped.cpp

--- RainWaterTrapped.cpp
+++ RainWaterTrapped.cpp
@@ -1,65 +1,45 @@
-vector<int> Solution::prevSmaller(vector<int> &A) {
-
-    vector<int>ans;
+int Solution::trap(const vector<int> &A) {
+    if(A.size()==0)
+    return 0;
     
-    stack<int>s;
+    int left=A[0];
     
-    for(int i=0;i<A.size();i++)
+    
+    vector<int> x(A.size());
+    x[0]=left;
+    
+    for(int i=1;i<A.size();i++)
     {
-      while(s.size()!=0&&s.top()>=A[i])
-      s.pop();
-        
-        if(s.size()==0)
-        {int Solution::trap(const vector<int> &arr){
+        left=max(left,A[i]);
+        x[i]=left;
+    }
     
-    int n=arr.size();
-    int left[n];
- 
-   
-    int right[n];
- 
-  
-    int water = 0;
- 
-   
-    left[0] = arr[0];
-    for (int i = 1; i < n; i++)
-       left[i] = max(left[i-1], arr[i]);
- 
-    // Fill right array
-    right[n-1] = arr[n-1];
-    for (int i = n-2; i >= 0; i--)
-       right[i] = max(right[i+1], arr[i]);
- 
-   .
-    for (int i = 0; i < n; i++)
-       water += min(left[i],right[i]) - arr[i];
- 
-    return water;
-     
-        
+    int n=A.size();
+    vector<int> y(n);
+    
+    int right=A[n-1];
+    y[n-1]=A[n-1];
+    
+    for(int i=n-2;i>-1;i--)
+    {
+        right=max(right,A[i+1]);
+        y[i]=right;
     }
     
     
     
+    int ans=0;
     
-    
-    
-    
-
-    
-
-
-            ans.push_back(-1);
-        }
-        else
-        {
-            ans.push_back(s.top());
-        }
-        s.push(A[i]);
-        
+    for(int i=0;i<n;i++)
+    {
+        int yo=min(x[i],y[i]);
+       // cout<<yo<<".";
+        if(yo>A[i])
+        ans=ans+yo-A[i];
     }
     
     return ans;
+    
+    
     
 }
